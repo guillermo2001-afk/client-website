412f01a6dad1956c352277f92565fb628ef8b747
Guillermo Hernandez-Nieves <123615578+guillermo2001-afk@users.noreply.github.com>
2024-04-15 20:59:55 -0400

    potential version of the website

---
+++ validation_functions.php
@@ -0,0 +1,12 @@
+<?php
+// Function to check text length
+function validateTextLength($text, $min, $max) {
+    $length = strlen($text);
+    return ($length >= $min && $length <= $max);
+}
+
+// Function to check number range
+function validateNumberRange($number, $min, $max) {
+    return ($number >= $min && $number <= $max);
+}
+?>
